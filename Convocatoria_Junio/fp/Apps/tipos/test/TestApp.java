e33dcced67d9e8d4f9387b2229992ec51d5c4394
ubidragon <ubahidarr@alum.us.es>
2016-06-14 15:14:50 +0200

    Creacion de casos de pruebas
    +App
    +AppTienda
    +Opinion
    +Tienda

---
+++ Convocatoria_Junio/fp/Apps/tipos/test/TestApp.java
@@ -0,0 +1,19 @@
+package fp.Apps.tipos.test;
+
+import fp.Apps.tipos.App;
+
+/**
+ * 14 jun. 2016
+ * @author Ubidragon
+ *
+ * ExamenesFp-1415
+ */
+public class TestApp {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Convocatoria_Junio/fp/Apps/tipos/test/TestTienda.java
@@ -0,0 +1,16 @@
+package fp.Apps.tipos.test;
+
+/**
+ * 14 jun. 2016
+ * @author Ubidragon
+ *
+ * ExamenesFp-1415
+ */
+public class TestTienda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
